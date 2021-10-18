3eb709ab3df05b21ec633288fe8e4b323f40f496
vishal.Alok <you@example.com>
2021-10-18 07:40:33 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Delegate
+{
+    public delegate void AddDelegate(int x, int y);
+    public delegate string HelloDelegate(string s);
+    class Program
+    {
+        public void add(int a, int b)
+        {
+            Console.WriteLine(a+b);
+        }
+        public static string hello(string name)
+        {
+            return ("Hello" + name);
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            AddDelegate obj1 = new AddDelegate(p.add);
+            HelloDelegate obj2 = new HelloDelegate(hello);
+            obj1(100, 50);
+            //or
+            obj1.Invoke(100, 20);
+
+            Console.WriteLine(obj2("Vishal"));
+            Console.WriteLine(obj2.Invoke("Abhay"));
+        }
+    }
+}
